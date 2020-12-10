f8d4b00adb180f5a962cc756b66b87ed59ed475a
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-12-10 12:11:30 +0200

    CS_advanced - expanded arrays and streams; CS_OOP - begin MySuperBank project - from C#101

---
+++ CS_advanced/Streams_Files_Directories/Odd_lines/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Odd_lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            var reader = new StreamReader("input.txt");
+
+            using (reader)
+            {
+                int counter = 0;
+
+                string line = reader.ReadLine();
+
+                using (var writer = new StreamWriter("output.txt"))
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+
+        }
+    }
+}
